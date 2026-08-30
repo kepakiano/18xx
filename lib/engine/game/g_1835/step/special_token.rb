dadbd3420301ceae1b60d21503bfa66a3a1f87cf
Sebastian Schmitz <schmitz.basti@googlemail.com>
2026-08-30 03:08:18 +0200

    [1835] implement tokening ability of the NF private

--- lib/engine/game/g_1835/step/special_token.rb
+++ lib/engine/game/g_1835/step/special_token.rb
@@ -8,9 +8,17 @@ module Engine
       module Step
         class SpecialToken < Engine::Step::SpecialToken
           def available_tokens(entity)
+            return available_tokens(@round.current_operator) if entity == @game.company_by_id('NF')
             return [] unless entity.corporation?
 
             entity.tokens_by_type
+          end
+
+          def adjust_token_price_ability!(entity, token, hex, city, special_ability: nil)
+            # No special treatment needed if the entity is a player. This happens in the case of NF
+            return [token, nil] if entity.player?
+
+            super
           end
         end
       end
